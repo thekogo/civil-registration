fe23eb524610928f85987a34fd3091b7db053c25
thekogo <thekogo@gmail.com>
2020-05-06 19:20:47 +0700

    edit config

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,15 +1,6 @@
 import firebase from 'firebase'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAM5W-gfm6LQiB19wbthZUlT1M1oNPYL4o",
-    authDomain: "civil-regis.firebaseapp.com",
-    databaseURL: "https://civil-regis.firebaseio.com",
-    projectId: "civil-regis",
-    storageBucket: "civil-regis.appspot.com",
-    messagingSenderId: "76254668988",
-    appId: "1:76254668988:web:4c7d1867be9772729988b9",
-    measurementId: "G-HYGHHEQXXD"
-};
+// config
 
 firebase.initializeApp(firebaseConfig);
 export default firebase;
